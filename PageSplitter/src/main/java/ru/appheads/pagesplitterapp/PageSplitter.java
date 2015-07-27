7ef0c43f9eae4ee813f48829ab2c6bd7c39a3531
wufasong <wufasong@139.com>
2015-07-27 16:54:43 +0800

    support language like Chinese, in which there is no space between words.

--- PageSplitter/src/main/java/ru/appheads/pagesplitterapp/PageSplitter.java
+++ PageSplitter/src/main/java/ru/appheads/pagesplitterapp/PageSplitter.java
@@ -18,6 +18,8 @@ public class PageSplitter {
     private SpannableStringBuilder currentLine = new SpannableStringBuilder();
     private SpannableStringBuilder currentPage = new SpannableStringBuilder();
     private int currentLineHeight;
+    private boolean bStartNewLine;
+    private boolean bNoSpaceLang = false;
     private int pageContentHeight;
     private int currentLineWidth;
     private int textLineHeight;
@@ -27,6 +29,13 @@ public class PageSplitter {
         this.pageHeight = pageHeight;
         this.lineSpacingMultiplier = lineSpacingMultiplier;
         this.lineSpacingExtra = lineSpacingExtra;
+    }
+    
+    public void append(String text, TextPaint textPaint, boolean noSpace) 
+    {
+    	bNoSpaceLang = noSpace;
+    	
+    	append(text, textPaint);
     }
 
     public void append(String text, TextPaint textPaint) {
@@ -62,10 +71,133 @@ public class PageSplitter {
             pageContentHeight = 0;
         }
     }
+    
+    private String getSuitableSubSentence(String appendedText, TextPaint textPaint, int offset)
+    {
+        int oneCharWidth = (int) Math.ceil(textPaint.measureText(appendedText.substring(offset, offset + 1)));
+        
+        bStartNewLine = false;
+        
+     	/*
+     	 * the appendedText should be placed in a new line.
+     	 */
+        if (offset == 0 && oneCharWidth + currentLineWidth > pageWidth)
+        {
+        	bStartNewLine = true;
+        }
+     	
+        /*
+         * the left space of current line.
+         */
+     	int leftLineWidth = ((offset != 0 || bStartNewLine) ? pageWidth : pageWidth - currentLineWidth);
+     	
+     	/*
+     	 * how many chars be able to be filled in the left space of current line.
+     	 */
+     	int leftCharNum = leftLineWidth / oneCharWidth;
+     	int totalLen = appendedText.length();
+     	int leftLen = totalLen - offset;
+     	
+     	/*
+     	 * Only one char is left.
+     	 */
+     	if (leftLen == 1)
+     	{
+     		return appendedText.substring(offset,  offset + 1);
+     	}
+     	else 
+     	{
+     		int start = offset;
+     		int end = totalLen;
+    		int textWidth;
+    		int textWidthR;
+    		textWidth = (int) Math.ceil(textPaint.measureText(appendedText, start, end));
+    		
+    		/*
+    		 * the left substring is not enough to fill one line.
+    		 */
+    		if (textWidth < leftLineWidth)
+    		{
+    			return appendedText.substring(offset);
+    		}
+    		
+         	while (true)
+        	{
+         		leftCharNum = (end + start) / 2 - offset;
+        		textWidth = (int) Math.ceil(textPaint.measureText(appendedText, offset, offset + leftCharNum));
+        		textWidthR = (int) Math.ceil(textPaint.measureText(appendedText, offset, offset + leftCharNum + 1));
+        		
+        		if (textWidth < leftLineWidth && textWidthR  >= leftLineWidth)
+        		{
+        			break;
+        		}
+        		else if (textWidthR < leftLineWidth)
+        		{
+        			start = offset + leftCharNum;
+        		}
+        		else
+        		{
+        			end =  offset  + leftCharNum;
+        		}
+        	}
+     	}
+    	
+    	return appendedText.substring(offset,  offset + leftCharNum);
+    }
+
+    private void appendSentence(String appendedText, TextPaint textPaint) {
+        int textWidth;
+        String subSentence;
+        
+    	int offset = 0;
+    	int len = appendedText.length();
+        	
+    	while (true)
+    	{
+           	subSentence = getSuitableSubSentence(appendedText, textPaint, offset);
+    		textWidth = (int) Math.ceil(textPaint.measureText(subSentence));
+        	
+        	/*
+        	 * if current line  already fully fill pageWidth, do as before. 
+        	 */
+        	if (bStartNewLine)
+        	{
+				checkForPageEnd();
+				appendLineToPage(textLineHeight);
+				appendTextToLine(subSentence, textPaint, textWidth);
+        	}
+        	else 
+        	{
+            	appendTextToLine(subSentence, textPaint, textWidth);
+        	}
+            	
+        	/*
+        	 * if the subsentence is the final part of appendedText, this appendedText handling should be finished.
+        	 */
+        	if (offset + subSentence.length() == len)
+        	{
+        		break;
+        	}
+        	
+        	if (bStartNewLine == false)
+        	{
+                checkForPageEnd();
+                appendLineToPage(textLineHeight);
+        	}
+            
+        	offset += subSentence.length();
+    	}
+    }
 
     private void appendWord(String appendedText, TextPaint textPaint) {
         int textWidth = (int) Math.ceil(textPaint.measureText(appendedText));
         if (currentLineWidth + textWidth >= pageWidth) {
+        	if (bNoSpaceLang)
+        	{
+            	appendSentence(appendedText, textPaint);
+            	return;
+        	}
+        	
             checkForPageEnd();
             appendLineToPage(textLineHeight);
         }
